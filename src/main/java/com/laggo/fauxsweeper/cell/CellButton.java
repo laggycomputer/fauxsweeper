9df266dabe17118ac9aeff831b991891a422381c
Dante Dam <laggycomputer@yahoo.com>
2023-02-17 16:51:51 -0800

    remove property for CellButton
    just use normal field

--- src/main/java/com/laggo/fauxsweeper/cell/CellButton.java
+++ src/main/java/com/laggo/fauxsweeper/cell/CellButton.java
@@ -1,24 +1,15 @@
 package com.laggo.fauxsweeper.cell;
 
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.control.Button;
 
 public class CellButton extends Button {
-    private ObjectProperty<ICell> cell;
+    private ICell cell;
 
-    public final ICell getCell() {
-        return cell == null ? null : cell.get();
+    public ICell getCell() {
+        return this.cell;
     }
 
-    public final void setCell(ICell cell) {
-        cellProperty().set(cell);
-    }
-
-    public final ObjectProperty<ICell> cellProperty() {
-        if (cell == null) {
-            cell = new SimpleObjectProperty<>(null);
-        }
-        return cell;
+    public void setCell(ICell cell) {
+        this.cell = cell;
     }
 }
